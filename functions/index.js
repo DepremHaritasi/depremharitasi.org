5b1de0250dea6e5639c3770ea7e04e95e61b95ae
Mehmet Kose <mehmetkosedev@gmail.com>
2023-02-09 01:06:30 +0300

    clean up function

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,8 @@
 const functions = require("firebase-functions");
 const firebaseAdmin = require("firebase-admin");
-const ngeohash = require("ngeohash");
 const geofire = require("geofire-common");
 
 require("dotenv").config();
-
-const Airtable = require("airtable");
-const baseId = process.env.BASE_ID;
-const tableId = process.env.TABLE_ID;
-const token = process.env.AIRTABLE_TOKEN;
-
-const base = new Airtable({ apiKey: token }).base(baseId);
-const table = base(tableId);
 
 const serviceAccount = require("./serviceAccountKey.json");
 firebaseAdmin.initializeApp({
@@ -20,108 +11,25 @@ firebaseAdmin.initializeApp({
 
 const firebaseAdminDB = firebaseAdmin.firestore();
 
-// import json file
-const depremyardimData = require("./outsourceData/depremyardim7.json");
-
-const getRecords = async () => {
-  const records = await table.select().all();
-  return records;
-};
-
-// exports.data = functions.https.onRequest(async (request, response) => {
-//   response.set("Access-Control-Allow-Origin", "*");
-//   response.set("Content-Type", "application/json");
-//   if (request.method !== "GET") {
-//     response.status(403).send("Forbidden!");
-//   }
-
-//   const records = await getRecords();
-//   const recordsJson = records
-//     .map((record) => {
-//       let data = record._rawJson;
-//       if (data.fields["Konum"] && data.fields["Konum"].includes(",")) {
-//         data.fields["Lat"] = parseFloat(
-//           data.fields["Konum"].split(",")[0].trim()
-//         );
-//         data.fields["lng"] = parseFloat(
-//           data.fields["Konum"].split(",")[1].trim()
-//         );
-//       }
-//       delete data.fields["Telefon"];
-//       return {
-//         ...data,
-//       };
-//     })
-//     .filter((record) => {
-//       const fields = Object.keys(record.fields);
-//       if (fields.includes("Konum") && record.fields["Konum"].includes(",")) {
-//         return true;
-//       }
-//       return false;
-//     });
-
-//   response.send({
-//     version: "0.1.0",
-//     records: recordsJson,
-//   });
-// });
-
-// exports.depremyardim = functions.https.onRequest(async (request, response) => {
-//   response.set("Access-Control-Allow-Origin", "*");
-//   response.set("Content-Type", "application/json");
-//   if (request.method !== "GET") {
-//     response.status(403).send("Forbidden!");
-//   }
-
-//   response.send({
-//     version: "7SUBAT_15:12",
-//     records: depremyardimData,
-//   });
-// });
-
-const getGeohashRange = (
-  latitude,
-  lngitude,
-  distance // miles
-) => {
-  const lat = 0.0144927536231884; // degrees latitude per mile
-  const lon = 0.0181818181818182; // degrees lngitude per mile
-
-  const lowerLat = latitude - lat * distance;
-  const lowerLon = lngitude - lon * distance;
-
-  const upperLat = latitude + lat * distance;
-  const upperLon = lngitude + lon * distance;
-
-  const lower = ngeohash.encode(lowerLat, lowerLon);
-  const upper = ngeohash.encode(upperLat, upperLon);
-
-  return {
-    lower,
-    upper,
-  };
-};
-
 exports.api = functions
   .runWith({
-    // Keep 5 instances warm for this latency-critical function
-    // minInstances: 3,
+    minInstances: 1,
     maxInstances: 5,
-    // increase ram
-    //memory: "512MB",
+    // memory: "512MB",
   })
   .https.onRequest(async (request, response) => {
     response.set("Access-Control-Allow-Origin", "*");
     response.set("Content-Type", "application/json");
     if (request.method !== "GET") {
-      response.status(403).send("Forbidden!");
+      return response.status(403).send("Forbidden!");
     }
+
     let { lat, lng } = request.query;
     lat = Number(lat);
     lng = Number(lng);
 
     if (!lat || !lng) {
-      response.send({ success: false });
+      return response.send({ success: false });
     }
 
     const center = [lat, lng];
@@ -174,7 +82,7 @@ exports.api = functions
       return matchingDocs;
     });
 
-    response.send({
+    return response.send({
       version: "0.1.1",
       lat,
       lng,
